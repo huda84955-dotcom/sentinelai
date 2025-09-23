81024247e4609dc4c34aec9cb9884d783b58d104
hudakhaleel <huda84955@gmail.com>
2025-09-23 21:16:16 +0300

    test2

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Step 1: Load audio
+file_path = "sample.wav"  # 
+y, sr = librosa.load(file_path, sr=16000)
+print(f"Audio shape: {y.shape}, Sample rate: {sr}")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-   # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Step 2: Extract MFCC features
+mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+print("MFCC shape:", mfcc.shape)
 
+# Step 3: Plot MFCC
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(mfcc, x_axis='time')
+plt.colorbar()
+plt.title("MFCC Features")
+plt.show()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-HI
-my name is hoda
